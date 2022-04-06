dc867db805745eb5caed4118f8307fc342a6852b
graytmatter <matt.t.gray@gmail.com>
2022-04-06 14:14:55 -0700

    changed hero image behavior, and nav bar shading

---
+++ docs/main.js
@@ -0,0 +1,22 @@
+function shadeNav(){
+    nav = document.getElementById("navigation");
+    if(window.innerWidth >= 550){
+        if(window.pageYOffset >= 700-46){
+            nav.style.background = "rgba(0, 0, 0, 1)"
+        }else{
+            nav.style.background= "rgba(0, 0, 0, 0.5)"
+        }
+    }else{
+        if(window.pageYOffset >= 350-92){
+            nav.style.background = "rgba(0, 0, 0, 0.9)"
+        }else{
+            nav.style.background= "rgba(0, 0, 0, 0.5)"
+        }
+    }
+}
+
+window.addEventListener("load", ()=>{
+    window.addEventListener("scroll", shadeNav)
+})
+
+
